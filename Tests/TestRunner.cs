6bf2106d7d56f3f25331c5bd95ff63b03f3e04a1
Cyber-Jager <torokhov.nickita@yandex.ru>
2021-07-21 02:23:11 +0700

    Setup NSpec testing

---
+++ Tests/TestRunner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NSpec;
+using NSpec.Domain;
+using NSpec.Domain.Formatters;
+
+namespace Tests
+{
+    class TestRunner
+    {
+        public static void Main(string[] args)
+        {
+            var tagOrClassName = string.Join(",", args);
+            var types = typeof(TestRunner).Assembly.GetTypes();
+            var finder = new SpecFinder(types, string.Empty);
+            var tagsFilter = new Tags().Parse(tagOrClassName);
+            var builder = new ContextBuilder(finder, tagsFilter, new DefaultConventions());
+            var runner = new ContextRunner(tagsFilter, new ConsoleFormatter(), false);
+            var results = runner.Run(builder.Contexts().Build());
+
+            Environment.Exit(results.Failures().Count());
+        }
+    }
+}
---
+++ Tests/Tests/describe_test.cs
@@ -0,0 +1,3 @@
+class describe_test : {
+    abc
+}
