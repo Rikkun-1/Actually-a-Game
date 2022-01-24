ca670b120e980896b6e41bcdc0c3055847d584cf
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-01-25 00:21:00 +0700

    Added OrthographicCameraController
    Took 58 minutes

---
+++ Assets/Scripts/Components/OrthographicCameraController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class OrthographicCameraController: MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float sizeChangeSpeed;
+    [Min(1)] public float defaultSize;
+    
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = GetComponentInChildren<Camera>();
+    }
+    
+    private void Update()
+    {
+        var angle = transform.rotation.eulerAngles;
+        
+        angle.y += Input.GetAxisRaw("YAxis") * rotationSpeed * Time.unscaledDeltaTime;
+
+        transform.rotation= Quaternion.Euler(angle);
+
+        var forwardVector = Quaternion.AngleAxis(angle.y, Vector3.up) * Vector3.forward;
+        var rightVector   = Quaternion.AngleAxis(angle.y, Vector3.up) * Vector3.right;
+        
+        var position = transform.position;
+        
+        var acceleratedMoveSpeed = moveSpeed * _camera.orthographicSize / defaultSize;
+
+        var vertical   = Input.GetAxisRaw("Vertical") * acceleratedMoveSpeed * Time.unscaledDeltaTime;
+        var horizontal = Input.GetAxisRaw("Horizontal") * acceleratedMoveSpeed * Time.unscaledDeltaTime;
+        
+        position += forwardVector * vertical;
+        position += rightVector * horizontal;
+
+        transform.position = position;
+
+        _camera.orthographicSize += Input.mouseScrollDelta.y * sizeChangeSpeed * Time.unscaledDeltaTime;
+        _camera.orthographicSize =  Mathf.Clamp(_camera.orthographicSize, 1, float.MaxValue);
+    }
+}
