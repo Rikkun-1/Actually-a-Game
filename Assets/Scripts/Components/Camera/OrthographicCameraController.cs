cd1a725023dd1fc130e6497caca49a1175376110
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-01-26 18:18:08 +0700

    OrthographicCameraController.cs refactored
    Took 1 hour 34 minutes

--- Assets/Scripts/Components/Camera/OrthographicCameraController.cs
+++ Assets/Scripts/Components/Camera/OrthographicCameraController.cs
@@ -2,9 +2,9 @@
 
 public class OrthographicCameraController: MonoBehaviour
 {
-    public float moveSpeed;
-    public float rotationSpeed;
-    public float sizeChangeSpeed;
+    public          float moveSpeed;
+    public          float rotationSpeed;
+    public          float sizeChangeSpeed;
     [Min(1)] public float defaultSize;
     
     private Camera _camera;
@@ -16,29 +16,36 @@ public class OrthographicCameraController: MonoBehaviour
     
     private void Update()
     {
-        var angle = transform.rotation.eulerAngles;
+        HandleRotation();
+        HandleMovement();
+        HandleZooming();
+    }
+    
+    private void HandleRotation()
+    {
+        var rotationDelta = Input.GetAxisRaw("YAxis") * rotationSpeed * Time.unscaledDeltaTime;
+        transform.Rotate(Vector3.up, rotationDelta, Space.World);
+    }
+    
+    private void HandleMovement()
+    {
+        var zoomAcceleratedMoveSpeed = moveSpeed * _camera.orthographicSize / defaultSize;
+
+        var move =  zoomAcceleratedMoveSpeed * Time.unscaledDeltaTime;
+
+        var angle         = transform.rotation.eulerAngles.y;
+        var forwardVector = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward;
+        var rightVector   = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.right;
         
-        angle.y += Input.GetAxisRaw("YAxis") * rotationSpeed * Time.unscaledDeltaTime;
+        var verticalMove   = forwardVector * Input.GetAxisRaw("Vertical") * move;
+        var horizontalMove = rightVector * Input.GetAxisRaw("Horizontal") * move;
 
-        transform.rotation= Quaternion.Euler(angle);
-
-        var forwardVector = Quaternion.AngleAxis(angle.y, Vector3.up) * Vector3.forward;
-        var rightVector   = Quaternion.AngleAxis(angle.y, Vector3.up) * Vector3.right;
-        
-        var position = transform.position;
-        
-        var acceleratedMoveSpeed = moveSpeed * _camera.orthographicSize / defaultSize;
-
-        var vertical   = Input.GetAxisRaw("Vertical") * acceleratedMoveSpeed * Time.unscaledDeltaTime;
-        var horizontal = Input.GetAxisRaw("Horizontal") * acceleratedMoveSpeed * Time.unscaledDeltaTime;
-        
-        position += forwardVector * vertical;
-        position += rightVector * horizontal;
-
-        transform.position = position;
-
-        var orthographicSize = _camera.orthographicSize;
-        orthographicSize         += Input.mouseScrollDelta.y * sizeChangeSpeed * Time.unscaledDeltaTime;
-        _camera.orthographicSize =  Mathf.Clamp(orthographicSize, 1, float.MaxValue);
+        transform.Translate(verticalMove + horizontalMove, Space.World);
+    }
+    
+    private void HandleZooming()
+    {
+        var orthographicSizeDelta = Input.mouseScrollDelta.y * sizeChangeSpeed * Time.unscaledDeltaTime;
+        _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize + orthographicSizeDelta, 1, float.MaxValue);
     }
 }
