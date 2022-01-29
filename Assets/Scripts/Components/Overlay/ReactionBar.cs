4bbdee9a803140ef55cf1aad25813d8a1a471fe4
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-01-29 08:35:53 +0700

    Fixed bug with reaction bar still progressing when reaction was dropped when target hide
    Took 46 minutes

--- Assets/Scripts/Components/Overlay/ReactionBar.cs
+++ Assets/Scripts/Components/Overlay/ReactionBar.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(ReactionStartTimeListener))]
 [RequireComponent(typeof(ReactionDelayListener))]
+[RequireComponent(typeof(ReactionStartTimeListener))]
+[RequireComponent(typeof(ReactionStartTimeRemovedListener))]
 public class ReactionBar : MonoBehaviour
 {
     public Image reloadingBarImage;
@@ -10,18 +11,33 @@ public class ReactionBar : MonoBehaviour
     private float _reactionStartTime;
     private float _reactionDelay;
 
+    private ReactionDelayListener            _cachedReactionDelayListener;
+    private ReactionStartTimeListener        _cachedReactionStartTimeListener;
+    private ReactionStartTimeRemovedListener _cachedReactionStartTimeRemovedListener;
+    
     private void Start()
     {
-        GetComponent<ReactionStartTimeListener>().OnReactionStartTimeChanged += UpdateReactionStartTime;
-        GetComponent<ReactionDelayListener>().OnReactionDelayChanged         += UpdateReactionDelay;
+        _cachedReactionDelayListener            = GetComponent<ReactionDelayListener>();
+        _cachedReactionStartTimeListener        = GetComponent<ReactionStartTimeListener>();
+        _cachedReactionStartTimeRemovedListener = GetComponent<ReactionStartTimeRemovedListener>();
+        
+        _cachedReactionDelayListener.OnReactionDelayChanged                     += UpdateReactionDelay;
+        _cachedReactionStartTimeListener.OnReactionStartTimeChanged             += UpdateReactionStartTime;
+        _cachedReactionStartTimeRemovedListener.OnAfterReactionStartTimeRemoved += Hide;
     }
 
     private void OnDestroy()
     {
-        GetComponent<ReactionStartTimeListener>().OnReactionStartTimeChanged -= UpdateReactionStartTime;
-        GetComponent<ReactionDelayListener>().OnReactionDelayChanged         -= UpdateReactionDelay;
+        _cachedReactionDelayListener.OnReactionDelayChanged                     -= UpdateReactionDelay;
+        _cachedReactionStartTimeListener.OnReactionStartTimeChanged             -= UpdateReactionStartTime;
+        _cachedReactionStartTimeRemovedListener.OnAfterReactionStartTimeRemoved -= Hide;
     }
 
+    private void Hide()
+    {
+        reloadingBarImage.enabled = false;
+    }
+    
     private void UpdateReactionDelay(float newReactionDelay)
     {
         _reactionDelay = newReactionDelay;
@@ -29,7 +45,8 @@ public class ReactionBar : MonoBehaviour
 
     private void UpdateReactionStartTime(float newReactionStartTime)
     {
-        _reactionStartTime = newReactionStartTime;
+        reloadingBarImage.enabled = true;
+        _reactionStartTime        = newReactionStartTime;
     }
 
     private void Update()
--- Assets/Source/Orders/Shoot/Components/ReactionStartTimeComponent.cs
+++ Assets/Source/Orders/Shoot/Components/ReactionStartTimeComponent.cs
@@ -3,6 +3,7 @@ using Entitas.CodeGeneration.Attributes;
 
 [Game]
 [Event(EventTarget.Self)]
+[Event(EventTarget.Self, EventType.Removed)]
 public sealed class ReactionStartTimeComponent : IComponent
 {
     public float value;
