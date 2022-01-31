7d81f17e7d26cee24dfc7444dbec3237bd138cb9
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-01-31 21:33:19 +0700

    Fixed bug with units making step back each time when any wall destroyed
    Took 2 hours 45 minutes

--- Assets/Source/Pathfinding/PathfindingSystems.cs
+++ Assets/Source/Pathfinding/PathfindingSystems.cs
@@ -3,7 +3,7 @@
     public PathfindingSystems(Contexts contexts)
     {
         Add(new WalkabilityMapSystems(contexts));
-        Add(new RecalculatePathSystem(contexts));
+        //Add(new RecalculatePathSystem(contexts));
         Add(new ProcessPathRequestsSystem(contexts));
     }
 }
