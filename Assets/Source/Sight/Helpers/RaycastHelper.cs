04402e7090b1411fa5fe4d394b802e3217fe74fc
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-02-06 02:39:02 +0700

    Code optimization. Now vision check passed only if there is no obstacle in front of the target
    Took 34 minutes

--- Assets/Source/Sight/Helpers/RaycastHelper.cs
+++ Assets/Source/Sight/Helpers/RaycastHelper.cs
@@ -6,26 +6,25 @@ public static class RaycastHelper
 
     static RaycastHelper()
     {
-        _layerMask.value = LayerMask.GetMask("Default", "Vision Check");
+        _layerMask.value = LayerMask.GetMask("Default");
     }
 
     public static bool IsInClearVision(Vector3 origin, Vector3 target)
     {
         var raycastDirection = target - origin;
-        return Physics.Raycast(origin, raycastDirection, Vector3.Distance(origin, target)) == false;
+        return Physics.Raycast(origin, raycastDirection, Vector3.Distance(origin, target), _layerMask) == false;
     }
 
-    public static bool IsInClearVision(Vector3 origin, GameEntity targetEntity)
+    public static bool IsInClearVision(in Vector3 origin, GameEntity targetEntity)
     {
         if (!targetEntity.hasUnityView) return false;
 
         var targetPosition   = targetEntity.worldPosition.value.WithY(1.4f);
+        var distance         = Vector3.Distance(origin, targetPosition);
+
         var raycastDirection = targetPosition - origin;
 
-        if (!Physics.Raycast(origin, raycastDirection, out var hitInfo, Vector3.Distance(origin, targetPosition), _layerMask)) return false;
-
-        var entity = hitInfo.collider.GetGameEntity();
-        return entity.id.value == targetEntity.id.value;
+        return !Physics.Raycast(origin, raycastDirection, distance, _layerMask);
     }
 
     public static bool IsInClearVision(GameEntity e, GameEntity targetEntity)
