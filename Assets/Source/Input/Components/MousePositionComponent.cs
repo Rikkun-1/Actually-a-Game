b5e38ef24760f106b30f774c7c7ae9b7318d1bdd
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-02-06 03:08:10 +0700

    Replaced GetGridClickSystem.cs with ProcessMouseInputSystem.cs which will handle both mouse clicks and movement
    Took 3 minutes

---
+++ Assets/Source/Input/Components/MousePositionComponent.cs
@@ -0,0 +1,10 @@
+﻿using Entitas;
+using Entitas.CodeGeneration.Attributes;
+using UnityEngine;
+
+[Input]
+[Unique]
+public sealed class MousePositionComponent : IComponent
+{
+    public Vector2 position;
+}
