b08b8e12b2b23f89026485e391eed0b4ed8c29e3
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-02-06 01:15:55 +0700

    Added missing file for Walkability map helpers refactoring
    Took 1 minute

--- Assets/Source/Extensions/Roy-T.AStar/GridPositionExtensions.cs
+++ Assets/Source/Extensions/Roy-T.AStar/GridPositionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Roy_T.AStar.Primitives;
+﻿using System;
+using Roy_T.AStar.Primitives;
 using UnityEngine;
 
 public static class GridPositionExtensions
@@ -6,5 +7,24 @@ public static class GridPositionExtensions
     public static Vector2Int ToVector2Int(this GridPosition position)
     {
         return new Vector2Int(position.X, position.Y);
+    }
+
+    public static GridPosition StepInDirection(this GridPosition from, Direction direction)
+    {
+        var x = from.X;
+        var y = from.Y;
+        
+        return direction switch
+        {
+            Direction.Top         => new GridPosition(x,     y + 1),
+            Direction.Bottom      => new GridPosition(x,     y - 1),
+            Direction.Right       => new GridPosition(x + 1, y),
+            Direction.Left        => new GridPosition(x - 1, y),
+            Direction.TopRight    => new GridPosition(x + 1, y + 1),
+            Direction.TopLeft     => new GridPosition(x - 1, y + 1),
+            Direction.BottomRight => new GridPosition(x + 1, y - 1),
+            Direction.BottomLeft  => new GridPosition(x - 1, y - 1),
+            _                     => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
     }
 }
