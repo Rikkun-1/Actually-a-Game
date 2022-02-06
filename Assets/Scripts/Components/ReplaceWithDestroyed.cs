6c501d8c4e88b89be6e86cc76dd1a60ba78b9945
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-02-06 19:23:51 +0700

    Fixed missing feature. Now walls are destroyed with physics representation
    Took 21 minutes

---
+++ Assets/Scripts/Components/ReplaceWithDestroyed.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ReplaceWithDestroyed : MonoBehaviour
+{
+    public GameObject replacementPrefab;
+
+    public void Replace()
+    {
+        Instantiate(replacementPrefab, transform.position, transform.rotation, transform.parent);
+        Destroy(gameObject);
+    }
+}
