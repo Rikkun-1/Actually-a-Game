dff17c0f567511f65a4fd44de0c799c68ed43e05
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-02-07 12:24:03 +0700

    Now units rotated towards movement direction
    Took 2 hours 53 minutes

---
+++ Assets/Source/Movement/Systems/RotateTowardsMovementDirectionSystem.cs
@@ -0,0 +1,27 @@
+﻿using Entitas;
+using ProceduralToolkit;
+
+public class RotateTowardsMovementDirectionSystem : IExecuteSystem
+{
+    private readonly IGroup<GameEntity> _entities;
+
+    public RotateTowardsMovementDirectionSystem(Contexts contexts)
+    {
+        _entities = contexts.game.GetGroup(GameMatcher.AllOf(GameMatcher.Velocity,
+                                                             GameMatcher.Vision)
+                                                      .NoneOf(GameMatcher.LookAtPositionOrder,
+                                                              GameMatcher.LookAtDirectionOrder,
+                                                              GameMatcher.LookAtEntityOrder,
+                                                              GameMatcher.Destroyed));
+    }
+
+    public void Execute()
+    {
+        foreach (var e in _entities)
+        {
+            var desiredAngle  = e.velocity.value.ToVector2XZ().ToAngle();
+            var angleDelta    = e.vision.turningSpeed * GameTime.deltaTime;
+            VisionHelper.RotateEntityVisionTowards(e, desiredAngle, angleDelta);
+        }
+    }
+}
