556a0eb217a16835d794dc549655c67d44622fb1
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-02-07 12:59:26 +0700

    Fixed bug where default wall still exist when destroyed variant is spawned
    Took 6 minutes
    
    
    Took 1 minute

--- Assets/Scripts/Components/ReplaceWithDestroyed.cs
+++ Assets/Scripts/Components/ReplaceWithDestroyed.cs
@@ -4,8 +4,10 @@ public class ReplaceWithDestroyed : MonoBehaviour
 {
     public GameObject replacementPrefab;
 
+    [ContextMenu("Replace")]
     public void Replace()
     {
+        gameObject.SetActive(false);
         Instantiate(replacementPrefab, transform.position, transform.rotation, transform.parent);
         Destroy(gameObject);
     }
