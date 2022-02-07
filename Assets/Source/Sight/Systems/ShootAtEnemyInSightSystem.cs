f36a123db595d9e34e9bc77cd84c4993c814eafb
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-02-07 19:27:36 +0700

    Optimized ShootAtEnemyInSightSystem.cs
    Took 8 minutes

--- Assets/Source/Sight/Systems/ShootAtEnemyInSightSystem.cs
+++ Assets/Source/Sight/Systems/ShootAtEnemyInSightSystem.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Entitas;
 using UnityEngine;
 
@@ -8,6 +10,11 @@ public class ShootAtEnemyInSightSystem : IExecuteSystem
     private readonly IGroup<GameEntity> _entities;
     private readonly IGroup<GameEntity> _possibleTargets;
 
+    private IEnumerable<GameEntity>[] _possibleTargetsByDistance = Array.Empty<IEnumerable<GameEntity>>();
+
+    private int _delayBetweenChecks = 15;
+    private int _frame;
+
     public ShootAtEnemyInSightSystem(Contexts contexts)
     {
         _entities = contexts.game.GetGroup(GameMatcher.AllOf(GameMatcher.TeamID,
@@ -15,14 +22,31 @@ public class ShootAtEnemyInSightSystem : IExecuteSystem
                                                       .NoneOf(GameMatcher.ShootAtEntityOrder));
 
         _possibleTargets = contexts.game.GetGroup(GameMatcher.AllOf(GameMatcher.TeamID)
-                                                             .NoneOf(GameMatcher.Dead));
+                                                             .NoneOf(GameMatcher.Destroyed));
     }
 
     public void Execute()
     {
-        foreach (var e in _entities.GetEntities())
+        _frame++;
+        if (_frame == _delayBetweenChecks)
         {
-            var possibleTargetsByDistance = GetPossibleTargetsByDistance(e);
+            _frame = 0;
+            FindTargets();
+        }
+    }
+
+    private void FindTargets()
+    {
+        var entities = _entities.GetEntities();
+        var length   = entities.Length;
+
+        FindPossibleTargets(length, entities);
+
+        for (var i = 0; i < length; i++)
+        {
+            var e = entities[i];
+
+            var possibleTargetsByDistance = _possibleTargetsByDistance[i];
             foreach (var targetEntity in possibleTargetsByDistance)
             {
                 if (RaycastHelper.IsInClearVision(e, targetEntity))
@@ -34,6 +58,12 @@ public class ShootAtEnemyInSightSystem : IExecuteSystem
         }
     }
 
+    private void FindPossibleTargets(int length, GameEntity[] entities)
+    {
+        if (_possibleTargetsByDistance.Length < length) _possibleTargetsByDistance = new IEnumerable<GameEntity>[length];
+        Parallel.For(0L, length, (i) => _possibleTargetsByDistance[i] = GetPossibleTargetsByDistance(entities[i]));
+    }
+
     private static bool IsInSameTeam(GameEntity e, GameEntity targetEntity)
     {
         return e.teamID.value == targetEntity.teamID.value;
@@ -42,7 +72,7 @@ public class ShootAtEnemyInSightSystem : IExecuteSystem
     private IEnumerable<GameEntity> GetPossibleTargetsByDistance(GameEntity e)
     {
         return _possibleTargets.GetEntities()
-                               .Where(targetEntity => !IsInSameTeam(e, targetEntity))
+                               .Where(targetEntity => !IsInSameTeam(e, targetEntity) && Vector2.Distance(e.gridPosition.value, targetEntity.gridPosition.value) < 50f)
                                .OrderBy(entity => Vector2.Distance(e.gridPosition.value, entity.gridPosition.value));
     }
 }
