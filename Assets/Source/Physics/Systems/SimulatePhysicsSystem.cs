fa37498d40d962507779e9c74845260a09f86ee4
Cyber-Jager <torokhov.nickita@yandex.ru>
2022-02-11 21:45:08 +0700

    Minor fix. Now physics simulation would run each simulation tick
    Took 2 minutes

--- Assets/Source/Physics/Systems/SimulatePhysicsSystem.cs
+++ Assets/Source/Physics/Systems/SimulatePhysicsSystem.cs
@@ -3,17 +3,13 @@ using UnityEngine;
 
 public class SimulatePhysicsSystem : IExecuteSystem
 {
-    private int _frameDelay = 2;
-    private int _frame;
-    
     public SimulatePhysicsSystem(Contexts contexts)
     {
     }
     
     public void Execute()
     {
-        if (++_frame < _frameDelay) return;
-        _frame = 0;
-        Physics.Simulate(GameTime.deltaTime * _frameDelay);
+        var deltaTime = GameTime.deltaTime;
+        if(deltaTime > 0) Physics.Simulate(deltaTime);
     }
 }
